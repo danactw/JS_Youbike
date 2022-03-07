6786cffc374b46bf6f3d671f3624a2f6e64a42fa
danachen <danac.tw@gmail.com>
2022-03-07 11:06:27 +0800

    add google map and modal elements

--- youbike.js
+++ youbike.js
@@ -109,7 +109,49 @@ document.addEventListener('DOMContentLoaded', ()=>{
   }
 
   searchBtn.addEventListener('click', displaySpot)
+
+  // Get the modal
+  var modal = document.getElementById("myModal");
+
+  // Get the button that opens the modal
+  var btn = document.getElementById("myBtn");
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on the button, open the modal
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
 })
+
+function initMap() {
+  // The location of Uluru
+  const uluru = { lat: 25.02605, lng: 121.5436 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 15,
+    center: uluru,
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+  });
+}
 
 // act: "1"
 // ar: "復興南路二段235號前"
